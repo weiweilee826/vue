45f1d4c9a15a71b34057d4c0bc1b6cb4658f1ec8
weiweilee826 <42629336+weiweilee826@users.noreply.github.com>
2020-03-04 11:35:40 +0800

    signin router

---
+++ src/components/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Login from './components/pages/Login.vue'
+import Index from './components/Index.vue'
+import VueRouter from 'vue-router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+const routes = [
+  { path: '/', component: Index },
+  { path: '/login', component: Login },
+]
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
