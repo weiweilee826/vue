a3044eaaa09aed0398bf7bc9152dda3306b05563
weiweilee826 <42629336+weiweilee826@users.noreply.github.com>
2020-03-06 02:17:42 +0800

    新增修改商品

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="text-right mt-4">
-      <button class="btn btn-primary">建立新的產品</button>
+      <b-button @click="edit({})">建立新的產品</b-button>
     </div>
     <table class="table mt-4">
       <thead>
@@ -24,10 +24,140 @@
             <span v-if="product.is_enabled">已啟用</span>
             <span v-else>未啟用</span>
           </td>
-          <td><button class="btn btn-outline-primary btn-sm">編輯</button></td>
+          <td>
+            <b-button v-b-modal.modal-1 @click="edit(product)">編輯</b-button>
+          </td>
         </tr>
       </tbody>
     </table>
+
+    <b-modal id="modal-1" :title="title" @ok="editOk">
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="form-group">
+              <label for="image">輸入圖片網址</label>
+              <input
+                type="text"
+                class="form-control"
+                id="image"
+                placeholder="請輸入圖片連結"
+                v-model="tempProduct.imageUrl"
+              />
+            </div>
+            <div class="form-group">
+              <label for="customFile"
+                >或 上傳圖片
+                <i class="fas fa-spinner fa-spin"></i>
+              </label>
+              <input
+                type="file"
+                id="customFile"
+                class="form-control"
+                ref="files"
+                @change="upload"
+              />
+            </div>
+            <img :src="tempProduct.imageUrl" class="img-fluid" alt="" />
+          </div>
+          <div class="col-sm-8">
+            <div class="form-group">
+              <label for="title">標題</label>
+              <input
+                type="text"
+                class="form-control"
+                id="title"
+                placeholder="請輸入標題"
+                v-model="tempProduct.title"
+              />
+            </div>
+
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="category">分類</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="category"
+                  placeholder="請輸入分類"
+                  v-model="tempProduct.category"
+                />
+              </div>
+              <div class="form-group col-md-6">
+                <label for="price">單位</label>
+                <input
+                  type="unit"
+                  class="form-control"
+                  id="unit"
+                  placeholder="請輸入單位"
+                  v-model="tempProduct.unit"
+                />
+              </div>
+            </div>
+
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="origin_price">原價</label>
+                <input
+                  type="number"
+                  class="form-control"
+                  id="origin_price"
+                  placeholder="請輸入原價"
+                  v-model="tempProduct.origin_price"
+                />
+              </div>
+              <div class="form-group col-md-6">
+                <label for="price">售價</label>
+                <input
+                  type="number"
+                  class="form-control"
+                  id="price"
+                  placeholder="請輸入售價"
+                  v-model="tempProduct.price"
+                />
+              </div>
+            </div>
+            <hr />
+
+            <div class="form-group">
+              <label for="description">產品描述</label>
+              <textarea
+                type="text"
+                class="form-control"
+                id="description"
+                placeholder="請輸入產品描述"
+                v-model="tempProduct.description"
+              ></textarea>
+            </div>
+            <div class="form-group">
+              <label for="content">說明內容</label>
+              <textarea
+                type="text"
+                class="form-control"
+                id="content"
+                placeholder="請輸入產品說明內容"
+                v-model="tempProduct.content"
+              ></textarea>
+            </div>
+            <div class="form-group">
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  id="is_enabled"
+                  v-model="tempProduct.is_enabled"
+                  true-value="1"
+                  false-value="0"
+                />
+                <label class="form-check-label" for="is_enabled">
+                  是否啟用
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </b-modal>
   </div>
 </template>
 
@@ -35,7 +165,9 @@
 export default {
   data() {
     return {
-      products: []
+      products: [],
+      tempProduct: {},
+      title: ""
     };
   },
   methods: {
@@ -45,8 +177,66 @@ export default {
       this.axios
         .get(API)
         .then(function(response) {
-          console.log(response);
           vm.products = response.data.products;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    editOk() {
+      var vm = this;
+      if (this.tempProduct.id) {
+        // 修改
+        const API = `${process.env.VUE_APP_HOST}/api/weiwei/admin/product/${this.tempProduct.id}`;
+        this.axios
+          .put(API, {
+            data: this.tempProduct
+          })
+          .then(function(response) {
+            console.log(response);
+            vm.getProduct();
+            vm.$bvModal.hide("modal-1");
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+      } else {
+        // 新增
+        const API = `${process.env.VUE_APP_HOST}/api/weiwei/admin/product`;
+        this.axios
+          .post(API, {
+            data: this.tempProduct
+          })
+          .then(function(response) {
+            console.log(response);
+            vm.getProduct();
+            vm.$bvModal.hide("modal-1");
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+      }
+    },
+    edit(product) {
+      this.$bvModal.show("modal-1");
+      this.tempProduct = product;
+      this.title = product.id ? "修改產品" : "新增產品";
+    },
+    upload(event) {
+      var formData = new FormData();
+      formData.append("file-to-upload", event.target.files[0]);
+
+      var vm = this;
+      const API = `${process.env.VUE_APP_HOST}/api/weiwei/admin/upload`;
+      this.axios
+        .post(API, formData, {
+          headers: { "Content-Type": "multipart/form-data" }
+        })
+        .then(function(response) {
+          console.log(response);
+          if (response.data.success) {
+            vm.$set(vm.tempProduct, "imageUrl", response.data.imageUrl);
+          }
         })
         .catch(function(error) {
           console.log(error);
