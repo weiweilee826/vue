c29c4bccd895279a2739a567d3f43027e235e6ba
weiwei <weiweilee826@gmail.com>
2020-03-18 11:56:07 +0800

    新增 商品詳情頁面

--- src/pages/Order.vue
+++ src/pages/Order.vue
@@ -8,7 +8,6 @@
         img-alt="Image"
         img-top
         tag="article"
-        
         class="mb-2"
         align="left"
         v-for="product in products"
@@ -21,7 +20,11 @@
           >現在只要 {{ product.price | price }} 元</b-card-text
         >
         <template v-slot:footer>
-          <b-button href="#" variant="primary" class="card-link"
+          <b-button
+            href="#"
+            variant="primary"
+            class="card-link"
+            @click="getProduct(product.id)"
             >查看更多</b-button
           >
           <b-button href="#" variant="primary" class="card-link"
@@ -30,6 +33,44 @@
         </template>
       </b-card>
     </b-card-group>
+
+    <b-modal id="modal" :title="product.title">
+      <template v-slot:default>
+        <img :src="product.imageUrl" class="img-fluid" alt="" />
+        <blockquote class="blockquote mt-3">
+          <p class="mb-0">{{ product.content }}</p>
+          <footer class="blockquote-footer text-right">
+            {{ product.description }}
+          </footer>
+        </blockquote>
+        <div class="d-flex justify-content-between align-items-baseline">
+          <del class="h6" v-if="product.price"
+            >原價 {{ product.origin_price | price }} 元</del
+          >
+          <div class="h4" v-if="product.price">
+            現在只要 {{ product.price | price }} 元
+          </div>
+        </div>
+        <select name="" class="form-control mt-3" v-model="product.qty">
+          <option :value="num" v-for="num in 10" :key="num">
+            選購 {{ num }} {{ product.unit }}
+          </option>
+        </select>
+      </template>
+      <template v-slot:modal-footer>
+        <div class="text-muted text-nowrap mr-3">
+          小計 <strong>{{ (product.qty * product.price) | price }}</strong> 元
+        </div>
+        <button
+          type="button"
+          class="btn btn-primary"
+          @click="addtoCart(product.id, product.num)"
+        >
+          <i class="fas fa-spinner fa-spin"></i>
+          加到購物車
+        </button>
+      </template>
+    </b-modal>
   </div>
 </template>
 
@@ -37,11 +78,12 @@
 export default {
   data() {
     return {
-      products: []
+      products: [],
+      product: {}
     };
   },
   methods: {
-    getProduct(page) {
+    getProducts(page) {
       this.isLoading = true;
       const vm = this;
       const API = `${process.env.VUE_APP_HOST}/api/wwlee/products?page=${page}`;
@@ -58,10 +100,29 @@ export default {
         .catch(function(error) {
           console.log(error);
         });
+    },
+    getProduct(id) {
+      const vm = this;
+      const API = `${process.env.VUE_APP_HOST}/api/wwlee/product/${id}`;
+      this.axios
+        .get(API)
+        .then(function(response) {
+          if (response.data.success) {
+            // response.data.product.qty = 1;
+            vm.product = response.data.product;
+            vm.$set(vm.product, 'qty', 1)
+            vm.$bvModal.show("modal");
+          } else {
+            vm.$bus.$emit("pop", response.data.message, "danger");
+          }
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     }
   },
   created() {
-    this.getProduct();
+    this.getProducts();
   }
 };
 </script>
