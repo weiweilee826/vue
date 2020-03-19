4930a5183867cdbb3b427789cd90b3b2213c51bf
weiwei <weiweilee826@gmail.com>
2020-03-20 01:17:25 +0800

    新增 結帳表格驗證

--- src/main.js
+++ src/main.js
@@ -1,28 +1,39 @@
-import Vue from 'vue'
-import App from './App.vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import './assets/dashboard.css'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import VueRouter from 'vue-router'
-import router from './router'
-import Loading from 'vue-loading-overlay';
-import 'vue-loading-overlay/dist/vue-loading.css';
-import price from './filter/price';
+import Vue from "vue";
+import App from "./App.vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "./assets/dashboard.css";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import VueRouter from "vue-router";
+import router from "./router";
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
+import price from "./filter/price";
+import { ValidationProvider, ValidationObserver, extend } from "vee-validate";
+import * as rules from "vee-validate/dist/rules";
+import { messages } from "vee-validate/dist/locale/zh_TW.json";
 
-Vue.use(VueRouter)
-Vue.use(VueAxios, axios)
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
-Vue.config.productionTip = false
-axios.defaults.withCredentials = true
-Vue.component('Loading',Loading)
-Vue.prototype.$bus = new Vue()
-Vue.filter('price',price)
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.config.productionTip = false;
+axios.defaults.withCredentials = true;
+Vue.component("Loading", Loading);
+Vue.prototype.$bus = new Vue();
+Vue.filter("price", price);
+Vue.component("ValidationProvider", ValidationProvider);
+Vue.component("ValidationObserver", ValidationObserver);
+Object.keys(rules).forEach(rule => {
+  extend(rule, {
+    ...rules[rule], // copies rule configuration
+    message: messages[rule] // assign message
+  });
+});
 
 new Vue({
   render: h => h(App),
   router
-}).$mount('#app')
+}).$mount("#app");
--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -51,6 +51,93 @@
         </tr>
       </tfoot>
     </table>
+    <ValidationObserver v-slot="{ handleSubmit ,invalid}">
+      <b-form @submit.prevent="handleSubmit(onSubmit)">
+        <ValidationProvider
+          name="姓名"
+          rules="required"
+          v-slot="{ errors, valid }"
+        >
+          <b-form-group id="input-group-1" label="姓名:" label-for="input-1">
+            <b-form-input
+              id="input-1"
+              v-model="form.user.name"
+              placeholder="請輸入姓名"
+              :state="valid"
+            ></b-form-input>
+            <b-form-invalid-feedback id="input-1">{{
+              errors[0]
+            }}</b-form-invalid-feedback>
+          </b-form-group>
+        </ValidationProvider>
+        <ValidationProvider
+          name="email"
+          rules="required|email"
+          v-slot="{ errors, valid }"
+        >
+          <b-form-group id="input-group-2" label="email" label-for="input-2">
+            <b-form-input
+              id="input-2"
+              v-model="form.user.email"
+              placeholder="請輸入email"
+              :state="valid"
+            ></b-form-input>
+            <b-form-invalid-feedback id="input-2">{{
+              errors[0]
+            }}</b-form-invalid-feedback>
+          </b-form-group>
+        </ValidationProvider>
+        <ValidationProvider
+          name="電話"
+          rules="required"
+          v-slot="{ errors, valid }"
+        >
+          <b-form-group id="input-group-3" label="電話:" label-for="input-3">
+            <b-form-input
+              id="input-3"
+              type="tel"
+              v-model="form.user.tel"
+              placeholder="請輸入電話"
+              :state="valid"
+            ></b-form-input>
+            <b-form-invalid-feedback id="input-3">{{
+              errors[0]
+            }}</b-form-invalid-feedback>
+          </b-form-group>
+        </ValidationProvider>
+        <ValidationProvider
+          name="地址"
+          rules="required"
+          v-slot="{ errors, valid }"
+        >
+          <b-form-group id="input-group-4" label="地址:" label-for="input-4">
+            <b-form-input
+              id="input-4"
+              v-model="form.user.address"
+              placeholder="請輸入地址"
+              :state="valid"
+            ></b-form-input>
+            <b-form-invalid-feedback id="input-4">{{
+              errors[0]
+            }}</b-form-invalid-feedback>
+          </b-form-group>
+        </ValidationProvider>
+        <ValidationProvider name="留言" rules="" v-slot="{ errors, valid }">
+          <b-form-group id="input-group-5" label="留言:" label-for="input-5">
+            <b-form-input
+              id="input-5"
+              v-model="form.message"
+              placeholder="請留言"
+              :state="valid"
+            ></b-form-input>
+            <b-form-invalid-feedback id="input-1">{{
+              errors[0]
+            }}</b-form-invalid-feedback>
+          </b-form-group>
+        </ValidationProvider>
+        <b-button :disabled="invalid" type="submit" variant="primary">Submit</b-button>
+      </b-form>
+    </ValidationObserver>
   </div>
 </template>
 
@@ -70,7 +157,7 @@ export default {
           tel: "0912346768",
           address: "kaohsiung"
         },
-        message: "這是留言" 
+        message: "這是留言"
       }
     };
   },
@@ -132,6 +219,9 @@ export default {
           console.log(error);
         });
     },
+    onSubmit() {
+      console.log(this.form);
+    }
   },
   created() {
     this.getCart();
